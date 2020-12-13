856e1ace5b977d67293ca5acc47d0549e9220f2f
Vlas <vlas.storozhev@gmail.com>
2020-12-13 16:50:10 +0200

    add sight model, add mocks data

---
+++ lib/domain/sight.dart
@@ -0,0 +1,16 @@
+class Sight {
+  String name;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  String type;
+  Sight({
+    this.name,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  });
+}
